451f35601110e06b005f4de087ff00dff4c1ffa1
Kevin Camargo <kjcamargo2012@gmail.com>
2023-03-31 20:50:06 -0400

    commit-history | UI | Simple header to make it look better
    + Added header
    - Removing material ui related packages

--- apps/ui/src/app/App.tsx
+++ apps/ui/src/app/App.tsx
@@ -1,12 +1,22 @@
 import { StrictMode } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
+import './app.module.css'
+import Header from './components/Header'
+
+const headerHeight = 85
 
 export default function App() {
   return (
     <StrictMode>
-      <div className="mx-4 p-6 max-w-3xl md:mx-auto">
-        <RouterProvider router={router} />
+      <div>
+        <Header />
+        <div
+          className="mx-4 p-6 max-w-3xl md:mx-auto"
+          style={{ paddingTop: headerHeight }}
+        >
+          <RouterProvider router={router} />
+        </div>
       </div>
     </StrictMode>
   )
